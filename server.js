0dfdd89014f08c2c962cc5920985078bf26c3782
mohamad-alkaadi <mohamadalkaadi@gmail.com>
2026-06-11 01:40:01 +0200

    updated db

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const dotenv = require("dotenv")
-dotenv.config({ path: "./config.env" })
-const connectToMongoDB = require("./utils/dbConnect")
-const app = require("./app")
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const connectToMongoDB = require("./utils/dbConnect");
+const app = require("./app");
 
-const dbUri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@main.caxv9ev.mongodb.net/?retryWrites=true&w=majority&appName=main`
-connectToMongoDB(dbUri)
+const dbUri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.vviw4xx.mongodb.net/?retryWrites=true&w=majority&appName=main`;
+connectToMongoDB(dbUri);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => [console.log(`server is running on port ${port}`)])
+app.listen(port, () => [console.log(`server is running on port ${port}`)]);
